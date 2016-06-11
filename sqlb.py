8c47610be9fe1d49ec4dc29661a6bb46012d1272
Paolo Pandini <pandini.paolo@icloud.com>
2016-06-11 15:24:47 +0200

    aggiunte modifiche al database

---
+++ sqlb.py
@@ -0,0 +1,46 @@
+# INSERT Command
+
+# import the sqlite3 library
+import sqlite3
+"""
+# create the connection object
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# insert data
+cursor.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+
+# commit the changes
+conn.commit()
+
+# close the database connection
+conn.close()
+
+
+Metodo alternativo, piu compatto
+import sqlite3
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+	c.execute("INSERT INTO population VALUES('New York City', 'NY', 8200000)")
+	c.execute("INSERT INTO population VALUES('San Francisco', 'CA', 800000)")
+"""
+
+# executemany() method
+
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+	# insert multiple records using a tuple
+	cities = [
+		('Boston', 'MA', 600000),
+		('Chicago', 'IL', 2700000), 
+		('Houston', 'TX', 2100000), 
+		('Phoenix', 'AZ', 1500000)
+		]
+	# insert data into table
+	c.executemany('INSERT INTO population VALUES(?, ?, ?)', cities)
+
+
+
